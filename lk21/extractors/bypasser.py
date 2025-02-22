ef00037edeaa75983612afaecde2135184479600
GK BOTZ <131459110+GK-BOTZ@users.noreply.github.com>
2025-02-22 21:43:13 +0530

    Update bypasser.py

--- lk21/extractors/bypasser.py
+++ lk21/extractors/bypasser.py
@@ -354,10 +354,12 @@ class Bypass(BaseExtractor):
                 result = recursive_choice(extractor, result)
         return result
 
+def safe_removeprefix(text, prefix):
+    return text[text.startswith(prefix) and len(prefix):]
 
-# lazy method
 allBypassPattern = []
 pattern = re.compile(r"regex *: *(.+)(?:\n|$)")
+
 for key, value in inspect.getmembers(Bypass):
     if key.startswith("bypass_") and key != "bypass_url":
         for urlPattern in pattern.findall(value.__doc__ or ""):
@@ -365,13 +367,19 @@ for key, value in inspect.getmembers(Bypass):
                 re.compile(urlPattern)
             )
             allBypassPattern.append(urlPattern)
+
         if (allPattern := Bypass.bypassPattern.get(key)):
             for rule in allPattern["pattern"]:
                 valid_regex = re.sub(
-                    r"\[.+?\][+*]\??|\\[a-zA-Z][+*]\??", "\[id\]", rule.pattern)
-                valid_regex = re.sub(r"\.[*+]\??", "\[any\]", valid_regex)
+                    r".+?[+*]\??|\a-zA-Z][+*]\??", "id", rule.pattern)
+                valid_regex = re.sub(r"\.[*+]\??", "any", valid_regex)
+
                 for url in exrex.generate(valid_regex):
-                    Bypass.bypassPattern[key]["support"].add(
-                        removeprefix(urlparse(url).netloc, "www."))
+                    try:
+                        netloc = urlparse(url).netloc
+                        clean_netloc = safe_removeprefix(netloc, "www.")
+                        Bypass.bypassPattern[key]["support"].add(clean_netloc)
+                    except ValueError:
+                        continue
 
 Bypass.allBypassPattern = re.compile(r"|".join(allBypassPattern))
